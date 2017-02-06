1b29617ff6b9e7e76203a4e3ddc052cd0d2c1963
tatsuroooK <tktktk0805@gmail.com>
2017-02-06 15:01:53 +0900

    Add files via upload

--- array_sort.php
+++ array_sort.php
@@ -4,6 +4,20 @@
 $array = [1, 30, 14, 23, 33, 40, 120];
 /********ここにコードを追加***************/
 
+$swap = 0;
+
+for($i = 1; $i <= sizeof($array); i++) {
+  if($array[$i] > $array[$i + 1]) {
+    $swap = $array[$i];
+    $array[$i] = $array[$i + 1];
+    $array[$i + 1] = $swap;
+  }
+
+echo $array;
+
+}
+
+
 /********ここまでコードを追加***************/
 // ここから下は、変更しないでください。
 foreach ($array as $value) {
